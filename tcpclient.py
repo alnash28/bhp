033d5b652d998069364c151fe1425175aae748fa
anash28 <alfredzo.nash@gmail.com>
2015-12-27 01:48:01 -0500

    first commit

---
+++ tcpclient.py
@@ -0,0 +1,20 @@
+#!/bin/python
+
+import socket
+
+target_host = "www.google.com"
+target_port = 80
+
+# create a socket
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# connect a client
+client.connect((target_host, target_port))
+
+# send some data
+client.send("GET /HTTP/1.1\r\nHost: google.com\r\n\r\n")
+
+# recieve some data
+response = client.recv(4096)
+
+print response
